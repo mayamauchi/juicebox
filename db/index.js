9597fe4e4875c4f8a782ebaf3e947ae59fb77c06
lucasawest <lucasawest1@gmail.com>
2022-10-28 10:07:03 -0600

    Part1 of Part 2

--- db/index.js
+++ db/index.js
@@ -294,6 +294,19 @@ async function getAllUsers() {
     }
   } 
 
+  async function getAllTags() {
+    try {
+      const { rows } = await client.query(`
+        SELECT * 
+        FROM tags;
+      `);
+  
+      return rows;
+    } catch (error) {
+      throw error;
+    }
+  }
+
  module.exports = {
     client,
     getAllUsers,
@@ -306,5 +319,6 @@ async function getAllUsers() {
     createTags,
     addTagsToPost,
     getPostsByUser,
-    getPostsByTagName
+    getPostsByTagName,
+    getAllTags
   }
