ef2d8096f57230cc0a47523054bb0bf63f67422d
lucasawest <lucasawest1@gmail.com>
2022-11-01 13:29:31 -0600

    Tags finish

--- api/tags.js
+++ api/tags.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const tagsRouter = express.Router();
+const { getPostsByTagName } = require("../db")
 
 tagsRouter.use((req, res, next) => {
     console.log("A request is being made to /tags");
@@ -16,5 +17,21 @@ tagsRouter.use((req, res, next) => {
       tags
     });
   });
+
+  tagsRouter.get('/:tagName/posts', async (req, res, next) => {
+    // read the tagname from the params
+    const { tagName } = req.params
+    console.log(tagName)
+
+    try {
+      // use our method to get posts by tag name from the db
+      const posts = await getPostsByTagName(tagName);
+      // send out an object to the client { posts: // the posts }
+      res.send({ posts: posts });
+    } catch ({ name, message }) {
+      // forward the name and message to the error handler
+      next({name, message});
+    }})
+  
 
 module.exports = tagsRouter
