1242795e5e5e62cf604ffaa0c5ab0877a6fc3d71
lucasawest <lucasawest1@gmail.com>
2022-11-01 07:33:21 -0600

    api posts

--- api/posts.js
+++ api/posts.js
@@ -5,8 +5,40 @@ const { getAllPosts } = require("../db");
 const { createPost } = require("../db");
 
 postsRouter.post("/", requireUser, async (req, res, next) => {
-  res.send({ message: "under construction" });
+  const { title, content, tags = "" } = req.body;
+  const authorId = req.user.id
+
+  const tagArr = tags.trim().split(/\s+/)
+  const postData = {authorId, title, content};
+
+  // only send the tags if there are some to send
+  if (tagArr.length) {
+    postData.tags = tagArr;
+  }
+
+  try {
+    const post = await createPost(postData)
+
+    if(post){res.send({post})}
+      else{
+        next({
+          name: "TestError",
+          message: "TestError",
+        })
+      }
+    // add authorId, title, content to postData object
+    // const post = await createPost(postData);
+    // this will create the post and the tags for us
+    // if the post comes back, res.send({ post });
+    // otherwise, next an appropriate error object 
+  } catch ({ name, message }) {
+    next({ name, message });
+  } 
 });
+
+
+ 
+
 
 postsRouter.use((req, res, next) => {
   console.log("A request is being made to /posts");
